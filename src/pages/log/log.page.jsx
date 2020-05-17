a91cd04e4427259e6282fd754dbc001ef04d4681
ekonine <xeon.slyde@gmail.com>
2020-05-17 18:24:27 -0500

    add styles to pages

--- src/pages/log/log.page.jsx
+++ src/pages/log/log.page.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center'
+  },
+}));
 
 export default function Logs() {
+
+  const classes = useStyles();
+
   return(
-    <div>
+    <div className={classes.root}>
       Log page
     </div>
   );
--- src/pages/manage/manage.page.jsx
+++ src/pages/manage/manage.page.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center'
+  },
+}));
 
 export default function Manage() {
+
+  const classes = useStyles();
+
   return(
-    <div>
+    <div className={classes.root}>
       Manage Page
     </div>
   );
