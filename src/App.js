f7d9df28c18750db4da8b6de585df57d8610a2a0
ekonine <xeon.slyde@gmail.com>
2020-05-17 15:19:19 -0500

    add pages with react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import NavBar from './components/nav-bar/nav-bar.component.jsx';
+import Home from './pages/home/home.page.jsx';
+import Manage from './pages/manage/manage.page.jsx';
+import Logs from './pages/log/log.page.jsx';
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/manage"> 
+          <Manage />
+        </Route>
+        <Route path="/logs">
+          <Logs />
+        </Route>
+      </Switch>
     </div>
   );
 }
