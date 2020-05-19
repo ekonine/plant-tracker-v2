5db80148817df431c34fe227fd44bcda8aaebb1f
ekonine <xeon.slyde@gmail.com>
2020-05-18 20:18:01 -0500

    add data to detail

--- src/pages/detail/detail.page.jsx
+++ src/pages/detail/detail.page.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {makeStyles} from '@material-ui/core/styles'; 
+import {makeStyles} from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import {useParams} from 'react-router-dom';
 
 //Styles
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
     flexWrap: 'wrap',
@@ -17,10 +17,10 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   paper: {
-    padding: theme.spacing(4)
+    padding: theme.spacing(4),
   },
   title: {
-    fontSize: 14
+    fontSize: 14,
   },
 }));
 
@@ -28,11 +28,35 @@ export default function Detail({plantData}) {
   const classes = useStyles();
   const {plantId} = useParams();
 
-  return(
+  return (
     <div className={classes.root}>
       <Paper className={classes.paper}>
-        <Typography className={classes.title} color="textPrimary" gutterBottom>        
+        <Typography className={classes.title} color="textPrimary" gutterBottom>
           {plantData[plantId].plantSpecies}
+        </Typography>
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom>
+          {plantData[plantId].plantNickname}
+        </Typography>
+        <Typography
+          className={classes.title}
+          color="textPrimary"
+          gutterBottom>
+          <b>House Location:</b> {plantData[plantId].plantHouseLoc}
+        </Typography>
+        <Typography
+          className={classes.title}
+          color="textPrimary"
+          gutterBottom>
+          <b>Bought from:</b> {plantData[plantId].plantBoughtLoc}
+        </Typography>
+        <Typography
+          className={classes.title}
+          color="textPrimary"
+          gutterBottom>
+          <b>Bought for:</b> {plantData[plantId].plantPrice} {plantData[plantId].plantPriceCurr}
         </Typography>
       </Paper>
     </div>
