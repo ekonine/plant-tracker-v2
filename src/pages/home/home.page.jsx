d7af82e02e5d827090b3c11a90eee70a0d9e93ca
ekonine <xeon.slyde@gmail.com>
2020-05-23 01:37:22 -0500

    create layout for home

--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import {makeStyles} from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
-    justifyContent: 'center'
+    flexDirection: 'column',
+    alignItems: 'center'
   },
+  large: {
+    marginTop: theme.spacing(2),
+    height: theme.spacing(40),
+    width: theme.spacing(40)
+  },
+  small: {
+    marginTop: theme.spacing(2),
+    height: theme.spacing(20),
+    width: theme.spacing(40)
+  },
+  text: {
+    marginTop: theme.spacing(2)
+  }
 }));
 
 export default function Home() {
@@ -14,7 +30,13 @@ export default function Home() {
 
   return(
     <div className={classes.root}>
-      Home Page
+      <Typography variant="h5" className={classes.text}>Dashboard</Typography>
+      <Paper className={classes.small} elevation={3}>
+      </Paper>
+      <Paper className={classes.large} elevation={3}>
+      </Paper>
+      <Paper className={classes.small} elevation={3}>
+      </Paper>
     </div>
   );
 }
